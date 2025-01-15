d8b47ac6918a1b6ca0a7b5ea9af3933c619e3691
JuliaCicer13 <137887271+JuliaCicer13@users.noreply.github.com>
2025-01-15 02:10:41 +0200

    h

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -2,9 +2,28 @@ import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
 const form = document.createElement('form');
+const input = document.createElement('input');
+input.type = 'text';
+input.name = 'search';
+input.placeholder = 'Enter search query...';
+form.appendChild(input);
 
 
-form.addEventListener("submit", () =>{
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  
+  const searchValue = input.value.trim();
+
+     if (searchValue === "") {
+       iziToast.show({
+         title: "Error",
+         message: "These fields are empty, please, fill these all!",
+       color: "red",})
+       return;
+  };
+  
+  
+
     fetch("https://jsonplaceholder.typicode.com/users")
     .then((response) => {
       if (!response.ok) {
@@ -15,9 +34,7 @@ form.addEventListener("submit", () =>{
     .then((users) => {
 			// Дані від бекенда
 			console.log(users);
-            if (users === "") {
-iziToast.show("Error: These fields are empty , please, fill these all!")
-            }
+         
 		})
     .catch((error) => console.log(error));
 });
