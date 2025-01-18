5db96bb2f25d68116ca95916601c6c7fdde685ca
JuliaCicer13 <137887271+JuliaCicer13@users.noreply.github.com>
2025-01-18 20:43:14 +0200

    p

---
+++ src/js/main.js
@@ -0,0 +1,64 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+const form = document.createElement('form');
+const input = document.createElement('input');
+const photoList = document.createElement('ul');
+
+input.type = 'text';
+input.name = 'search';
+input.placeholder = 'Enter search query...';
+form.appendChild(input);
+
+const MY_API_KEY = "48269071-e04b626909c4b310c40a55ae0";
+
+const params = new URLSearchParams({
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: 'true',
+
+})
+
+form.addEventListener("submit", (event) => {
+
+
+  event.preventDefault();
+  
+  const searchValue = input.value.trim();
+
+     if (searchValue === "") {
+       iziToast.show({
+         title: "Error",
+         message: "These fields are empty, please, fill these all!",
+       color: "red",})
+       return;
+  };
+   
+  
+
+    fetch(`https://pixabay.com/api/?key=${MY_API_KEY}&q=${searchValue}&${params}`)
+    .then((response) => {
+      if (!response.ok) {
+          throw new Error(response.status);
+        }
+         return response.json();
+      })
+     
+    .then((data) => {
+      // Дані від бекенда
+        if (data.hits.length === 0) {
+            iziToast.show({
+              title: "Error",
+              message: "Sorry, there are no any matching your search query. Please try again!",
+              color: "red",
+        })
+            return;
+       }
+  
+        photoList.insertAdjacentHTML =
+            createMarkUp(data.hits);
+		})
+        .catch((error) =>
+            console.log(error));
+});
+
--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,61 +1,37 @@
-import iziToast from "izitoast";
-import "izitoast/dist/css/iziToast.min.css";
+const MY_API_KEY = '48269071-e04b626909c4b310c40a55ae0';
+const searchValue = 'cats';
+const params = new URLSearchParams({
 
-const form = document.createElement('form');
-const input = document.createElement('input');
-const photoList = document.createElement('ul');
+  image_type: 'photo',
+  orientation: 'horizontal',
+  safesearch: true,
 
-input.type = 'text';
-input.name = 'search';
-input.placeholder = 'Enter search query...';
-form.appendChild(input);
-
-
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  
-  const searchValue = input.value.trim();
-
-     if (searchValue === "") {
-       iziToast.show({
-         title: "Error",
-         message: "These fields are empty, please, fill these all!",
-       color: "red",})
-       return;
-  };
-  
-  
-
-    fetch("https://pixabay.com/api/?key={ KEY }&q=yellow+flowers&image_type=photo")
-    .then((response) => {
-      if (!response.ok || array === "") {
-        iziToast.show({
-          titel: "Error",
-          message: "Sorry, there are no images matching your search query. Please try again!",
-          color: "red",
-        })
-      }
-      return response.json();
-    })
-    .then((photo) => {
-      // Дані від бекенда
-       const markup = photo
-    .map((photo) => {
-         
-      return
-
-
-
-    })
-    .join("");
-  
-  photoList.insertAdjacentHTML("beforeend", markup);
-			console.log(photo);
-         
-		})
-    .catch((error) => console.log(error));
 });
 
+fetch(`https://pixabay.com/api/?key=${MY_API_KEY}&q=${searchValue}&${params}`)
+  .then((response) => {
+    if (!response.ok) {
+      iziToast.show({
+        title: "Error",
+        message: "Сеть не в порядке",
+        color: "red",
+     
+      });
+
+      return response.json();
+
+
+    }
+    then((data)=> {
+      console.log(data);
+
+    })
+      .catch((error) => {
+  
+        console.log.error('Ошибка: error')
+})
+
+})
 
 
 
